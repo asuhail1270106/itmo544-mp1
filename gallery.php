8f4a7c7c05737e0e3e3d37f694b84333a2873716
asuhail1270106 <asuhail@hawk.iit.edu>
2017-10-14 18:13:38 -0500

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="CSS/style.css" type="text/css" media="screen" />
+    <meta charset="utf-8" />
+    <title>Resume</title>
+    <script type= 'text/javascript' src='js/jquery-1.12.4.js'></script>
+    <script type= 'text/javascript' src='js/RotatingPictureDisplay.js'></script>
+  </head>
+  <body>
+    <div id = "bodyspecs">
+      <div id = "image-wrap">       
+        <img class="images"
+	     src="http://www.thedjservice.com/blog/wp-content/uploads/2012/11/resume.jpg" 
+	     alt="Resume">
+	<img class="images"
+             src="https://www.freshers360.com/wp-content/uploads/2016/02/resume-writing.jpg" 
+	     alt="Resume2">
+	<img class="images"
+	     src="http://logosolusa.com/wp-content/uploads/parser/Sign-Work-Logo-1.jpg" 
+	     alt="Working">
+	<img class="images"
+ 	     src="http://bowriversolutions.com/wp-content/uploads/2014/04/Worker-Icon.jpg"
+	     alt="Deskjob">
+      </div>
+      <header>
+        <nav class="center">
+          <ul id = "list-nav">
+            <li><a href="index.php">Index</a></li>
+            <li><a href="gallery.php">Resume</a></li>
+            <li><a href="submit.php">Spring 2017</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div id = "bodyspecs2">
+        <section id = "resumecontactinfo">
+          <h1> Abdullah Suhail </h1>	  	
+          <p> Cell Number: 630-518-1224  </p>
+          <p> Personal Email: reachabdu@gmail.com </p>
+          <p> Student Email: asuhail@hawk.iit.edu </p>
+        </section>
+        <footer id="ContactInfo">
+          <address>
+	    <p>Get in touch with me:</p>
+	    <p>Personal Email: <a href="mailto:reachabdu@gmail.com">reachabdu@gmail.com</a></p>
+	    <p>Academic E-mail: <a href="mailto:asuhail@hawk.iit.edu">asuhail@hawk.iit.edu</a></p>
+	    <p>Visit me at my local library: 4 Friendship Plaza, Addison, IL 60101</p>
+	    <p>Coded by Abdullah Suhail (2017)</p>
+          </address>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
